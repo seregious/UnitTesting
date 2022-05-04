bb7fc287e08e1399b02e9784a9bcefd3de898308
seregious <seregious@gmail.com>
2022-05-04 12:34:46 +0300

    Initial Commit

---
+++ UnitTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnitTesting
+//
+//  Created by Сергей Иванчихин on 04.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
